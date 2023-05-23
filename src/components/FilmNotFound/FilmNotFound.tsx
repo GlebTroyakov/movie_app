5afc25c6b607cf56a01f04d5b281dbca2fcce6c4
GlebTroyakov <glebtroyakov@gmail.com>
2023-05-23 17:23:43 +0300

    added mobile version

--- src/components/FilmNotFound/FilmNotFound.tsx
+++ src/components/FilmNotFound/FilmNotFound.tsx
@@ -1,14 +1,8 @@
 import { Alert } from 'antd'
-import CSS from 'csstype'
+import './FilmNotFound.css'
 
 export const FilmNotFound = function ({ textError }: { textError: string }) {
-  const styleDivNotFound: CSS.Properties = {
-    margin: '20px auto',
-    textAlign: 'center',
-    width: '500px',
-  }
-
   const alert = <Alert message="Error" description={textError} type="error" showIcon />
 
-  return <>{textError && <div style={styleDivNotFound}>{alert}</div>}</>
+  return <>{textError && <div className="film-not-found">{alert}</div>}</>
 }
--- src/components/NotNetwork/NotNetwork.tsx
+++ src/components/NotNetwork/NotNetwork.tsx
@@ -1,19 +1,13 @@
 import { Alert } from 'antd'
-import CSS from 'csstype'
 import { Offline } from 'react-detect-offline'
+import './NotNetwork.css'
 
 export const NotNetwork = function () {
-  const styleDivNotFound: CSS.Properties = {
-    margin: '20px auto',
-    textAlign: 'center',
-    width: '500px',
-  }
-
   const alert = <Alert message="Error" description="Not Network" type="error" showIcon />
 
   return (
     <Offline>
-      <div style={styleDivNotFound}>{alert}</div>
+      <div className="not-network">{alert}</div>
     </Offline>
   )
 }
