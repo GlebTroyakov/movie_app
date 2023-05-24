8279b5c32c7951448d0a029650da3749e15d22f7
GlebTroyakov <glebtroyakov@gmail.com>
2023-05-24 16:41:52 +0300

    added props polling in offline

--- src/components/NotNetwork/NotNetwork.tsx
+++ src/components/NotNetwork/NotNetwork.tsx
@@ -6,7 +6,7 @@ export const NotNetwork = function () {
   const alert = <Alert message="Error" description="Not Network" type="error" showIcon />
 
   return (
-    <Offline>
+    <Offline polling={false}>
       <div className="not-network">{alert}</div>
     </Offline>
   )
